142f5059a97a5bddb74eb18f065a3f4be72955e6
Blackscure <34754398+Blackscure@users.noreply.github.com>
2025-01-06 01:35:18 +0300

    aside created

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -32,10 +32,12 @@ const navigation = [
         <ThemeSwitcher />
       </div>
       <nav class="mt-4">
-        <router-link v-for="item in navigation" 
-                    :key="item.name"
-                    :to="item.to"
-                    class="flex items-center px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">
+        <router-link 
+          v-for="item in navigation" 
+          :key="item.name"
+          :to="item.to"
+          class="flex items-center px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+          :class="{ 'bg-gray-200 dark:bg-gray-700 font-bold': $route.path === item.to }">
           <component :is="item.icon" class="w-5 h-5 mr-2" />
           {{ item.name }}
         </router-link>
